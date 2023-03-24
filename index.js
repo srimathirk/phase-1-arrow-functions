f7e039d63646de78c1bae90167cfa32385283598
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-03-24 11:21:09 -0400

    Completed lab

---
+++ index.js
@@ -0,0 +1,6 @@
+const divide = (a,b) => {return (a/b);}
+console.log(divide (2000,100));
+const square = x => x**2;
+console.log(square(8));
+const add = (num1,num2)=>{return num1+num2}
+console.log(add(5,9));
